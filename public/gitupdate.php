f3bce9b5f250baee9f21cd46fbcba07534ae8fcb
WinglessWhistle <58287456+WinglessWhistle@users.noreply.github.com>
2021-02-28 01:18:00 +1100

    fixed gitupdate

--- public/gitupdate.php
+++ public/gitupdate.php
@@ -1,14 +1,10 @@
 <?php
 $output=null;
-$retval=null;
-echo ('cd into directory');
-exec('cd /home/WinglessWhistle/VirialXYZ/', $output, $retval);
-echo "Returned with status $retval and output:\n";
+echo ("cd into directory\n");
+$output=shell_exec('cd /home/WinglessWhistle/VirialXYZ/');
 print_r($output);
 $output=null;
-$retval=null;
-echo ('pulls changes from github');
-exec('git pull', $output, $retval);
-echo "Returned with status $retval and output:\n";
+echo ("pulls changes from github\n");
+$output=shell_exec('git pull');
 print_r($output);
 ?>
