f4889065b117279d63ca1f3dac4c6234aca04376
WinglessWhistle <58287456+WinglessWhistle@users.noreply.github.com>
2021-02-28 02:34:40 +1100

    added secret

--- public/gitupdate.php
+++ public/gitupdate.php
@@ -1,10 +1,13 @@
 <?php
-$output=null;
-echo ("cd into directory\n");
-$output=shell_exec('cd /home/WinglessWhistle/VirialXYZ/');
-print_r($output);
-$output=null;
-echo ("pulls changes from github\n");
-$output=shell_exec('git pull');
-print_r($output);
+require(__DIR__ . "/vendor/autoload.php");
+
+use GitHubWebhook\Handler;
+
+$handler = new Handler("killme", __DIR__);
+shell_exec('cd /home/WinglessWhistle/VirialXYZ/');
+if($handler->handle()) {
+    echo "OK";
+} else {
+    echo "Wrong secret";
+}
 ?>
